dfbb0a163e4353b7a0e6ede5a24599fed9297034
Tom S. Bertalan <tom@tombertalan.com>
2012-11-12 20:23:12 -0500

    Making graphs.

--- hw4.py
+++ hw4.py
@@ -13,6 +13,7 @@ dt = dx * dy / 4 / kappa
 print 'dt is', dt
 print 'tmax is', tmax
 Nsteps = int(np.ceil((tmax - t0) / dt))
+print 'Nsteps is', Nsteps
 T = np.empty((Nx, Ny, Nsteps))
 print T.shape
 
@@ -34,14 +35,21 @@ for k in xrange(Nsteps-1):
     for j in xrange(1, Ny - 1):
         T[-1, j, k + 1] = T[0, j, k + 1]
 
-print np.linalg.norm(T[:,:,-1])
-#import matplotlib.pyplot as plt
-#fig1 = plt.figure(1, figsize=(12, 8))
-#ax1 = fig1.add_subplot(1,1,1)
-#ax1.scatter(T[:,:,-1])
+
+import matplotlib.pyplot as plt
+import sys
+fig1 = plt.figure(1, figsize=(3, 2), dpi=100)
+ax1 = fig1.add_subplot(1,1,1)
+for k in xrange(80-1):
+    ax1.imshow(T[:,:,k])
+    filename = 'heatmaps/heatmap_t%s.png' % str(k).zfill(4)
+    sys.stdout.write('saving: ' + filename + ' of ' + '%i' % Nsteps + '\r')
+    sys.stdout.flush()
+    plt.savefig(filename)
 #plt.show()
 
-from mayavi.mlab import contour_surf
-x, y = np.mgrid[x0:dx:xmax, y0:dy:ymax]
-s = contour_surf(x, y, T[:, :, -1])
-s.scene.save('test.png')
+#from mayavi.mlab import contour_surf
+#x, y = np.mgrid[x0:dx:xmax, y0:dy:ymax]
+#s = contour_surf(x, y, T[:, :, -1])
+#s.scene.save('test.png')
+#
