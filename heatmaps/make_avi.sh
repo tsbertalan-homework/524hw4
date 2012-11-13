f63efae9ba918b2a7151f9c64195f59ef03690af
Tom S. Bertalan <tom@tombertalan.com>
2012-11-13 09:45:38 -0500

    mp4, actually

--- heatmaps/make_avi.sh
+++ heatmaps/make_avi.sh
@@ -1,2 +1,13 @@
+#!/bin/bash
+startdir=`pwd`
+if [ `basename $startdir` = 'heatmaps' ]
+then
+    date >> /tmp/yup
+else
+    cd heatmaps
+fi
 mencoder 'mf://heatmap_t*.png' -mf type=png:fps=10 -ovc lavc -oac copy -o animation.avi
-
+avconv -y -i animation.avi animation.mp4 
+rm animation.avi
+rm heatmap_t*.png
+cd $startdir
