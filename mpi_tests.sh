1682de8b214e73a9851395e81702c6d25358790f
Tom S. Bertalan <tom@tombertalan.com>
2012-11-14 13:55:04 -0600

    Added command-line ability for testing purposes.

--- mpi_tests.sh
+++ mpi_tests.sh
@@ -1,12 +1,27 @@
 #!/usr/bin/env sh
 make mpi
-for NX in 32 64 128 256
+FILENAME=time_results.csv
+if [ $# -ne 1 ]
+then
+    echo "USAGE: `basename $0` [\"list of nx values\"]"
+    echo "multiples of 8 are recommended."
+    NX_LIST="64 256 128 32"
+else
+    NX_LIST=$1
+fi
+echo "proceeding with this list: $NX_LIST"
+for NX in $NX_LIST
 do
-    for N_PROCS in 1 2 4 8
+    echo ""
+    echo "NX=$NX"
+    for N_PROCS in 8 2 4 1
     do
-        echo "For $N_PROCS processors:" | tee -a time_results.txt
+        echo "N_PROCS=$N_PROCS"
         COMMAND="mpirun -np $N_PROCS hw4-mpi.exe $NX"
-        /usr/bin/time -f "%C %E" --output=time_output $COMMAND
-        cat time_output | tee -a time_results.txt
+        /usr/bin/time -f "$NX $N_PROCS %e" --output=time_output $COMMAND
+        echo ""
+        cat time_output | tee -a $FILENAME
     done
 done
+echo " " | tee -a $FILENAME
+echo "DONE" | tee -a $FILENAME
