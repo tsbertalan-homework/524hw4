8d5455139b0328d4e0fb2dd40f1717d6b3766632
Tom S. Bertalan <tom@tombertalan.com>
2012-11-14 17:35:28 -0500

    This is only suitable for output from omp_tests.sh or mpi_tests.sh

--- graph_times.py
+++ graph_times.py
@@ -13,21 +13,40 @@ nproccol=1
 timecol=2
 sizes = list(set(list(T[:,sizecol])))
 nprocs = list(set(list(T[:,nproccol])))
-print T
-print sizes
-print nprocs
-first_index_per_size = {}
+#print T
+
+size_indices = []
 for size in sizes:
-    first_index_per_size[size] = list(T[:, sizecol]).index(size)
-print first_index_per_size
-sys.exit()
+    size_indices.append(list(T[:, sizecol]).index(size))
 fig1 = plt.figure(1, figsize=(6, 4), dpi=100)
 ax1 = fig1.add_subplot(1, 1, 1)
-ax1.imshow(T, cmap='hot', interpolation='nearest')
-CS = ax1.contour(y, x, T.T)
-plt.clabel(CS, inline=1, fontsize=10)
-filename = 'heatmap_from_cpp.png'
-plt.savefig(filename)
+colors = ['red', 'blue', 'green', 'yellow']
+for i in range(len(size_indices)):
+    if i == len(size_indices)-1:
+        final = T.shape[0]
+    else:
+        final = size_indices[i+1]
+    procs =  T[size_indices[i]:final,nproccol]
+    times =  T[size_indices[i]:final,timecol]
+    try:
+        procs, times = zip(*sorted(zip(procs, times)))
+    except:
+        pass
+    ax1.plot(procs, times, color=colors[i])
+filename = 'times.png'
+plt.legend(["NX = %i" % x for x in sizes])
+ax1.set_xlabel('nprocs')
+ax1.set_ylabel('times [s]')
+
+#locs, labels = plt.xticks()
+#print locs
+#print list(labels)
+#labels = ['{0} {0}','{0} {0}', '{0} {0}']
+#new_labels = [x.format(locs[i]) for i,x  in enumerate(labels)]
+ax1.set_xlim([0,20])
+plt.xticks([1, 2, 4, 8], ['1', '2', '4', '8'])
+
+#plt.savefig(filename)
 plt.show()
 
 
