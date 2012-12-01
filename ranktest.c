7b5e229d8d113321e336e21ffa96bae6621075bc
Tom S. Bertalan <tom@tombertalan.com>
2012-12-01 15:44:03 -0500

    moved definitions before MPI_Init()

--- ranktest.c
+++ ranktest.c
@@ -1,11 +1,11 @@
 #include <mpi.h>
 #include <stdio.h>
 int main(int argc, char *argv[]){
-    MPI_Init(NULL, NULL);
     int world_rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     int world_size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Init(NULL, NULL);
     printf("rank %d\n", world_rank);
     printf("size %d\n", world_size);
     MPI_Finalize();
