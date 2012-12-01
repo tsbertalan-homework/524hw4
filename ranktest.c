9fd22e0938168451d9a47edb135d1414e3a557f6
Tom S. Bertalan <tom@tombertalan.com>
2012-12-01 15:46:01 -0500

    only declarations before init (pointless, I know)

--- ranktest.c
+++ ranktest.c
@@ -3,9 +3,9 @@
 int main(int argc, char *argv[]){
     int world_rank;
     int world_size;
+    MPI_Init(NULL, NULL);
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-    MPI_Init(NULL, NULL);
     printf("rank %d\n", world_rank);
     printf("size %d\n", world_size);
     MPI_Finalize();
